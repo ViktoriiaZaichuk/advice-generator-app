4b035c321ed62c4b92b08e487a178b5e8807c826
Vika Zaichuk <viktoriia.zaichuk@gmail.com>
2022-06-12 22:10:40 +0200

    Project setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+
+      </header>
+
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://vikazaichuk.com">Vika Zaichuk</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
