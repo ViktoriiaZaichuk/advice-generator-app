eb5958b7a3a192163173ca06a142833b79588c9d
Vika Zaichuk <viktoriia.zaichuk@gmail.com>
2022-06-21 15:53:59 +0200

    Added animation

--- src/App.js
+++ src/App.js
@@ -6,15 +6,12 @@ import { useEffect, useState } from 'react';
 import { getAdvice } from './api/Api';
 
 function App() {
-  const [advice, setAdvice] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [advice, setAdvice] = useState({});
 
   useEffect(() => {
     getAdvice()
       .then((data) => {
           setAdvice(data.slip);
-          setIsLoaded(true);
-          console.log(data.slip)
       });
   }, []);
 
@@ -23,23 +20,22 @@ function App() {
   }
 
   return ( 
-    <div>
+    <>
       <div className="card-container">
         <div className="advice-nr">ADVICE #{advice && advice.id}</div>
         <div className="advice-txt">{advice && advice.advice}</div>
         
-        <div><img src={PatternDivider} alt="Pattern Divider"/></div>
+        <div><img src={PatternDivider} className="img-pattern" alt="Pattern Divider"/></div>
 
         <div className='advice-dice' onClick={refreshPage}><img src={Dice} alt="Dice"/></div>
 
       </div>
 
-      
       <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
         Coded by <a href="https://vikazaichuk.com">Vika Zaichuk</a>.
       </div>
-    </div>
+    </>
   );
 }
 
